3b68a5863ffa6b6d03c6023a879ef7b7f5af1aac
jesseawilson <jaw97756@gmail.com>
2014-05-11 16:42:51 -0700

    put blocks back together, changed read logic, added 4th rd_addr/wr_addr byte in place of prev

--- homework3/src/fifo.sv
+++ homework3/src/fifo.sv
@@ -16,9 +16,8 @@ module fifo (
 	output           full      //full flag
 	);
 
-	reg	[2:0]	rd_addr;
-	reg     [2:0]	wr_addr;
-	reg		prev; //1 for write, 0 for read
+	reg	[3:0]	rd_addr;
+	reg     [3:0]	wr_addr;
 	reg	[7:0]	wr_byte;
 
 	reg	[7:0]	byte0;
@@ -31,12 +30,75 @@ module fifo (
 	reg	[7:0]	byte7;
 
 
-//write counter
+
+
+//write to memory logic
 always_ff @(posedge wr_clk, negedge reset_n)  
 begin
-	if(!reset_n)	wr_addr <= '0;
-	else if(wr)	wr_addr <= wr_addr + 1;  //may cause issue by incrementing durring writing
-	else		wr_addr <= wr_addr;
+	if(!reset_n)	begin
+			wr_addr <= '0;
+  			byte0   <= '0;
+			byte1   <= '0;
+			byte2   <= '0;
+			byte3   <= '0;
+			byte4   <= '0;
+			byte5   <= '0;
+			byte6   <= '0;
+			byte7   <= '0;
+			end
+	
+	else if(wr)	begin
+			wr_byte <= data_in;
+
+			unique case (wr_addr[2:0])
+			    3'b000 : byte0 = wr_byte;
+			    3'b001 : byte1 = wr_byte; 
+		    	    3'b010 : byte2 = wr_byte; 
+		    	    3'b011 : byte3 = wr_byte; 
+		    	    3'b100 : byte4 = wr_byte; 
+		    	    3'b101 : byte5 = wr_byte; 
+		    	    3'b110 : byte6 = wr_byte; 
+		    	    3'b111 : byte7 = wr_byte; 
+			endcase
+
+			wr_addr <=  wr_addr + 1;
+			end
+	
+	else		begin
+			wr_byte <= wr_byte;
+			wr_addr <= wr_addr;
+			end
+end
+
+
+//read from memory logic
+always_ff @(posedge rd_clk, negedge reset_n)  
+begin
+	if(!reset_n)	begin
+			rd_addr  <= '0;
+			data_out <= byte0;
+			end
+			
+	else if(rd)	begin
+			unique case (rd_addr[2:0])
+			    3'b000 : data_out = byte0;
+			    3'b001 : data_out = byte1;
+			    3'b010 : data_out = byte2;
+			    3'b011 : data_out = byte3;
+			    3'b100 : data_out = byte4;
+			    3'b101 : data_out = byte5;
+			    3'b110 : data_out = byte6;
+			    3'b111 : data_out = byte7;
+			endcase
+
+			rd_addr <= rd_addr + 1;
+			end
+
+	else		begin 
+			rd_addr  <= rd_addr;
+			data_out <= data_out;
+			end
+			
 end
 
 
@@ -49,84 +111,29 @@ begin
 end
 
 
-//write to memory logic
-always_ff @(posedge wr_clk, negedge reset_n)  
-begin
-	if(!reset_n)	begin
-  			byte0   <= '0;
-			byte1   <= '0;
-			byte2   <= '0;
-			byte3   <= '0;
-			byte4   <= '0;
-			byte5   <= '0;
-			byte6   <= '0;
-			byte7   <= '0;
-			end
-	
-	else if(wr)	begin
-			unique case (wr_addr)
-			    3'b000 : byte0 = wr_byte;
-			    3'b001 : byte1 = wr_byte; 
-		    	    3'b010 : byte2 = wr_byte; 
-		    	    3'b011 : byte3 = wr_byte; 
-		    	    3'b100 : byte4 = wr_byte; 
-		    	    3'b101 : byte5 = wr_byte; 
-		    	    3'b110 : byte6 = wr_byte; 
-		    	    3'b111 : byte7 = wr_byte; 
-			endcase
-
-			wr_byte <= data_in;
-			end
-	
-	else		wr_byte <= wr_byte;
-end
-
-
-//read from memory logic
-always_ff @(posedge rd_clk, negedge reset_n)  
-begin
-	if(!reset_n)	data_out <= '0;
-
-	else		begin
-			unique case (rd_addr)
-			    3'b000 : data_out = byte0;
-			    3'b001 : data_out = byte1;
-			    3'b010 : data_out = byte2;
-			    3'b011 : data_out = byte3;
-			    3'b100 : data_out = byte4;
-			    3'b101 : data_out = byte5;
-			    3'b110 : data_out = byte6;
-			    3'b111 : data_out = byte7;
-			endcase
-			end
-end
-
-
-
-
 //full and empty flag logic
 always_comb  
 begin
 	if(!reset_n)	begin
-			empty = 1;
-			full  = 0;
+			empty = 1'b1;
+			full  = 1'b0;
 			end
 
-	else if(rd_addr == wr_addr) begin
-		if(prev) begin
-			 full  = 1;
-			 empty = 0;
+	else if(rd_addr[2:0] == wr_addr[2:0]) begin
+		if(rd_addr[3] == wr_addr[3]) begin
+			 full  = 1'b0;
+			 empty = 1'b1;
 			 end
 
 		else	 begin
-			 empty = 1;
-			 full  = 0;
+			 empty = 1'b1;
+			 full  = 1'b0;
 			 end
 		end
 	
 	else	begin
-		empty = 0;
-		full  = 0;
+		empty = 1'b0;
+		full  = 1'b0;
 		end
 
 end
